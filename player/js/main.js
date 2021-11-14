f2e0e506f6a956db26dabb6806a1f3e020010600
poppi <s.dellaspora@outlook.com>
2021-11-14 10:45:03 +0100

    finalized aesthethic changes

--- player/js/main.js
+++ player/js/main.js
@@ -1,3 +1,13 @@
+//funzione globale per mettere in pause video e audio
+function playPause(toPause, toPlay){
+    if ( toPause.play()){
+        toPause.pause();
+        toPlay.play();
+    }else{
+        toPlay.play();
+    }
+}
+
 // funzioni per i video
 // chiamata ajax
 $.ajax({
@@ -8,10 +18,11 @@ $.ajax({
         //avvia funzione sul file json
         var videos = data.video;
 
+        //funzione che crea bottoni dinamici
         $.each(videos,function(any, some){
             var element = $('#template-bottoni div').clone();
             $(element)
-            .attr('data-src',path + some.file)
+            .attr('data-src', path + some.file)
             .text(some.title);
 
             var template = $('#template-bottoni').html();
@@ -20,17 +31,18 @@ $.ajax({
             .replace('##src##', path + some.file)
             .replace('##titolo##', some.title);
 
-            $('.altri-video-container').append(el)
-        })
+            $('.altri-video-container').append(el);
+        });
     }
 });
 
 $('.altri-video-container').on('click','div',function(any){
     any.preventDefault;
 
-    var clicksrc= $(this).attr('data-src');
+    var clicksrc = $(this).attr('data-src');
     myVid.src = clicksrc;
     myVid.load();
+    playPause(myaudio, myVid);
 })
 
 // funzioni per gli audio
@@ -44,27 +56,40 @@ $.ajax({
         var songs = data.audio;
 
         $.each(songs, function(any,some){
+            
             // clono il bottone
-            var a=$('#bottoni-canzoni div').clone();
-
-            $(a)
-            .attr('data-src',percorso + some.filo)
-            .text(some.title);
-
+            var clone = $('#bottoni-canzoni div').clone();
             var interfaccia = $('#bottoni-canzoni').html();
             var percorso = 'assets/media/audio/';
-            var a = interfaccia
+
+            $(clone)
+            .attr('data-src', percorso + some.filo)
+            .text(some.title);
+
+            var clone = interfaccia
             .replace('##source##', percorso + some.filo)
             .replace('##heading##', some.title);
 
-            $('.altre-canzoni-container').append(a);
+            $('.altre-canzoni-container').append(clone);
+        });
+
+        //funzione toggle class 
+        var bottoni = $('.bottoni');
+
+        bottoni.click(function(){
+            $(this).toggleClass('selected');
+            bottoni.not(this).removeClass('selected');
         })
     }
 });
 
 $('.altre-canzoni-container').on('click','div',function(e){
     e.preventDefault();
+
     var sourceCliccato = $(this).attr('data-src');
     myaudio.src = sourceCliccato;
+    
     myaudio.load();
+    playPause(myVid, myaudio);
+
 })
